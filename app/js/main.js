d799fdbda8fe226dc3b16c2571dd9aef8377ea5a
booobooka <skriblle@ukr.net>
2020-04-08 11:46:51 +0300

    added product slider product

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
 $(function(){
-
+    $(".rate-star").rateYo({
+          rating: 5,
+          starWidth: "12px",
+          readOnly: true
+    });
     var mixer = mixitup('.products-inner-container'); 
+    $('.product-slider-inner').slick({
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+    });
 });
