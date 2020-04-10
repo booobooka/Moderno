551d0fdbbd90d302a72edba0085cf2d75fa83fb3
booobooka <skriblle@ukr.net>
2020-04-10 19:26:36 +0300

    added page new items

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,37 @@
-$(function(){
+$(function () {
     $(".rate-star").rateYo({
-          rating: 5,
-          starWidth: "12px",
-          readOnly: true
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
     });
-    var mixer = mixitup('.products-inner-container'); 
+    var mixer = mixitup('.products-inner-container');
+    
     $('.product-slider-inner').slick({
         dots: true,
         slidesToShow: 4,
         slidesToScroll: 4,
-        arrows: false,
+        arrows: false
     });
+   
+    
 });
+$(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+});
+
+    $('.icon-th-list').on('click', function(){
+        $('.product-page__items .product-item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+    
+    $('.icon-th-large').on('click', function(){
+        $('.product-page__items .product-item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
