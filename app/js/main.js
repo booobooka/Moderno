81462645ee79dce785db019d46264b463005deca
booobooka <skriblle@ukr.net>
2020-04-17 11:07:21 +0300

    added 404, about, contact us, product one

--- app/js/main.js
+++ app/js/main.js
@@ -4,34 +4,58 @@ $(function () {
         starWidth: "12px",
         readOnly: true
     });
-    var mixer = mixitup('.products-inner-container');
-    
+
     $('.product-slider-inner').slick({
         dots: true,
         slidesToShow: 4,
         slidesToScroll: 4,
         arrows: false
     });
-   
-    
-});
-$(".js-range-slider").ionRangeSlider({
-    type: "double",
-    min: 0,
-    max: 1000,
-    from: 0,
-    to: 600,
-    prefix: "$"
-});
 
-    $('.icon-th-list').on('click', function(){
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function () {
         $('.product-page__items .product-item').addClass('list');
         $('.icon-th-list').addClass('active');
         $('.icon-th-large').removeClass('active');
     });
-    
-    $('.icon-th-large').on('click', function(){
+
+    $('.icon-th-large').on('click', function () {
         $('.product-page__items .product-item').removeClass('list');
         $('.icon-th-large').addClass('active');
         $('.icon-th-list').removeClass('active');
     });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu-list').slideToggle();
+    });
+
+
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
+
+    $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
+
+
+
+
+
+    var mixer = mixitup('.products-inner-container');
+
+});
+
